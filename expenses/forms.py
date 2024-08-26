69738ecfc0dc442c43b1d0b6f4e52d0cf905a196
Alfiya <gajsinaalfi@yandex.ru>
2024-08-27 01:37:36 +0300

    0.1.0 Adding expenses and categories

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from django import forms
 from .models import Expense, ExpenseCategory
 
@@ -8,8 +10,15 @@ class AddExpenseForm(forms.ModelForm):
     amount = forms.CharField(max_length=100)
     category = forms.ModelChoiceField(queryset=ExpenseCategory.objects.all())
     price = forms.IntegerField()
-    date = forms.DateTimeField()
 
     class Meta:
         model = Expense
-        fields = ('name', 'description', 'amount', 'category', 'price', 'date')
+        fields = ('name', 'description', 'amount', 'category', 'price')
+
+class AddCategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=50)
+    description = forms.CharField(max_length=180)
+
+    class Meta:
+        model = ExpenseCategory
+        fields = ('name', 'description')
--- expenses/views.py
+++ expenses/views.py
@@ -1,10 +1,7 @@
 from lib2to3.fixes.fix_input import context
 from django.shortcuts import render
 from expenses.models import Expense, ExpenseCategory
-from expenses.forms import AddExpenseForm
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-# Create your views here.
+from expenses.forms import AddExpenseForm, AddCategoryForm
 
 def index(request):
     context = {
@@ -15,26 +12,35 @@ def index(request):
     return render(request, 'expenses/index.html', context)
 
 def expenses(request):
+    category_form = AddCategoryForm(prefix='category_form')
+    expense_form = AddExpenseForm(prefix='expense_form')
+    if request.method == 'POST':
+        if 'expense_form' in request.POST:
+            expense_form = AddExpenseForm(data=request.POST, prefix='expense_form')
+            expense = Expense()
+            expense.name = request.POST['name']
+            expense.amount = request.POST['amount']
+            expense.price = request.POST['price']
+            expense.category = ExpenseCategory.objects.get(id=request.POST['category'])
+            expense.description = request.POST['description']
+            expense.save()
+            category_form = AddCategoryForm(prefix='category_form')
+
+        elif 'category_form' in request.POST:
+            category_form = AddCategoryForm(data=request.POST, prefix='category_form')
+            category = ExpenseCategory()
+            category.name = request.POST['name']
+            category.description = request.POST['description']
+            category.save()
+            expense_form = AddExpenseForm( prefix='expense_form')
+    else:
+        expense_form = AddExpenseForm(prefix='expense_form')
+        category_form = AddCategoryForm(prefix='category_form')
     context = {
         'title': 'Список трат',
         'expenses': Expense.objects.all(),
         'categories': ExpenseCategory.objects.all(),
-    }
-    return render(request, 'expenses/expenses.html', context)
-
-def add_expense(request, pk):
-    expense = get_object_or_404(Expense, pk=pk)
-    if request.method == 'POST':
-        form = AddExpenseForm(request.POST)
-        expense.name = form['name']
-        expense.amount = form['amount']
-        expense.price = form['price']
-        expense.category = form['category']
-        expense.description = form['description']
-        expense.date = expense['date']
-        expense.save()
-    context = {
-        'form': form,
-        'expense': expense,
+        'category_form': category_form,
+        'expense_form': expense_form,
     }
     return render(request, 'expenses/expenses.html', context)
